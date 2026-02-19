a095d1141521648c555bda663e8354cf55b5d369
redhatpeter <petertlee03@gmail.com>
2026-02-19 10:32:36 -0500

    testing 2nd workflow

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 def main():
     print("Hello, World from GitHub Actions!")
     print("This is a simple Python workflow example.")
-    print("✅ Workflow executed successfully!")
-    
+    print("this is the second time going thru the workflow")
+    print("✅ Workflow executed successfully!")    
 if __name__ == "__main__":
     main()
